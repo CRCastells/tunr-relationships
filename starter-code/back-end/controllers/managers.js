c5381fc74f0de349f71537cc0f52a65ee7a912b5
CRCastells <cameroncastells@gmail.com>
2017-11-17 08:42:31 -0700

    soln

--- starter-code/back-end/controllers/managers.js
+++ starter-code/back-end/controllers/managers.js
@@ -1,47 +1,49 @@
 var db = require('../models');
 var Manager = db.models.Manager;
+let Artist = db.models.Artist;
+let Ad = db.models.Ad;
 
 function index(req, res) {
-	Manager.findAll().then(function(managers) {
-		res.json(managers);
-	});
+  Manager.findAll({include:Ad}).then(function(managers) {
+    res.json(managers);
+  });
 }
 
 function show(req, res) {
-  Manager.findById(req.params.id)
-  .then(function(manager){
-    if(!manager) res.send("manager not found");
-    else res.json(manager);
-  });	
+  Manager.findById(req.params.id, { include: Artist })
+    .then(function(manager) {
+      if (!manager) res.send("manager not found");
+      else res.json(manager);
+    });
 }
 
 function create(req, res) {
-	Manager.create(req.body).then(function(manager){
-    if(!manager) res.send("manager not saved");
+  Manager.create(req.body).then(function(manager) {
+    if (!manager) res.send("manager not saved");
     else res.json(manager);
   });
 }
 
 function update(req, res) {
   Manager.findById(req.params.id)
-  .then(function(manager){
-    if(!manager) res.send("manager not found");
-    else return manager.updateAttributes(req.body);
-  })
-  .then(function(manager){
-    res.json(manager);
-  });
+    .then(function(manager) {
+      if (!manager) res.send("manager not found");
+      else return manager.updateAttributes(req.body);
+    })
+    .then(function(manager) {
+      res.json(manager);
+    });
 }
 
 function destroy(req, res) {
   Manager.findById(req.params.id)
-  .then(function(manager){
-    if(!manager) res.send("manager not found");
-    else return manager.destroy();
-  })
-  .then(function(){
-    res.send("manager deleted");
-  });  
+    .then(function(manager) {
+      if (!manager) res.send("manager not found");
+      else return manager.destroy();
+    })
+    .then(function() {
+      res.send("manager deleted");
+    });
 }
 
 module.exports.index = index;
--- starter-code/back-end/db/seed.js
+++ starter-code/back-end/db/seed.js
@@ -1,36 +1,77 @@
 var DB = require("../models").models;
 
- var artistCreate = function() {
- 	return DB.Artist.create({
-     name: 'Luciano Pavarotti',
-     photoUrl: 'http://img.informador.com.mx/biblioteca/imagen/677x508/811/810055.jpg',
-     nationality: 'Italiano',
-     instrument: 'Voice',
-     home_address: '1 Strada Roma'
-   });
- };
- 
- var managerCreate = function() {
- 	return DB.Manager.create({
-     name: 'Ricky Bobby',
-     email: 'rbobby@gmail.com',
-     office_number: '516-877-0304',
-     cell_phone_number: '718-989-1231'
- 	});
- };
- 
- var songCreate = function() {
- 	return DB.Song.create({
- 	    title: 'The Best Song Ever',
- 	    duration: '3:31',
- 	    date_of_release: '7/13/2015',
- 	    album_title: 'Best Album Ever'
- 	});
- };
- 
- artistCreate()
- .then(managerCreate)
- .then(songCreate)
- .then(function() {
- 	process.exit();
- }); 
+var lucySongs = [{
+    title: "O sole mio",
+    duration: "3:21",
+    date_of_release: "1990",
+    album_title: "Three Tenors in Concert",
+    artistId: ""
+  },
+  {
+    title: "Nessun dorma",
+    duration: "3:21",
+    date_of_release: "1990",
+    album_title: "Three Tenors in Concert",
+    artistId: ""
+  }
+];
+
+var artistCreate = function(manager_id) {
+     console.log('MANAGER ID: ' + manager_id);
+  return DB.Artist.create({
+    name: 'Luciano Pavarotti',
+    photoUrl: 'http://img.informador.com.mx/biblioteca/imagen/677x508/811/810055.jpg',
+    nationality: 'Italiano',
+    instrument: 'Voice',
+    home_address: '1 Strada Roma',
+    managerId: manager_id
+  }).then(artist => {
+    lucySongs.forEach(song => {
+      song.artistId = artist.id;
+    });
+    DB.Song.bulkCreate(lucySongs);
+  });
+};
+
+var managerCreate = function() {
+  return DB.Manager.create({
+      name: 'Ricky Bobby',
+      email: 'rbobby@gmail.com',
+      office_number: '516-877-0304',
+      cell_phone_number: '718-989-1231',
+    })
+    .then(manager => {
+      artistCreate(manager.dataValues.id);
+      adCreate(manager.dataValues.id);
+    });
+};
+
+var songCreate = function() {
+  return DB.Song.create({
+    title: 'The Best Song Ever',
+    duration: '3:31',
+    date_of_release: '7/13/2015',
+    album_title: 'Best Album Ever'
+  });
+};
+
+let adCreate = (manager_id) => {
+  return DB.Ad.create({
+    headline:'BEST MANAGER EVEAR',
+    url:'http://www.google.com',
+    managerId: manager_id
+  });
+};
+
+
+managerCreate()
+  .then(songCreate)
+  .then(() => {
+    process.exit();
+  });
+// artistCreate()
+//   .then(managerCreate)
+//   .then(songCreate)
+//   .then(function() {
+//     process.exit();
+//   });
